826bc083f10d85131fb7760020904f0170669447
Ujjwal <2204ujjwalkumar@gmail.com>
2026-01-30 17:58:22 +0530

    Added QR download button

--- qr_website/app.py
+++ qr_website/app.py
@@ -1,22 +1,26 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request
 import qrcode
+import base64
 import io
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
+    qr_image = None
+
     if request.method == "POST":
         url = request.form["url"]
 
         img = qrcode.make(url)
         buf = io.BytesIO()
         img.save(buf, format="PNG")
-        buf.seek(0)
 
-        return send_file(buf, mimetype="image/png")
+        # Convert image to base64 to show in HTML
+        img_base64 = base64.b64encode(buf.getvalue()).decode("utf-8")
+        qr_image = img_base64
 
-    return render_template("index.html")
+    return render_template("index.html", qr_image=qr_image)
 
 if __name__ == "__main__":
     app.run()
